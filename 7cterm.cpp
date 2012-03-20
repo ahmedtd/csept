43095107f3718eb53e6b75009001cba802575c94
reflores <rfloresmeath@tamu.edu>
2012-03-20 16:55:14 -0500

    Update 7cterm to have more of a framework.
    The updates include an integer-based menu to select actions. Only event actions are currently included. Additional ones are easy to hack in. The Find Event action might get deleted as well.
    
    Compile with g++ 7cterm.cpp -o 7cterm -lical
    I omitted the library in the last update, but it does matter

--- 7cterm.cpp
+++ 7cterm.cpp
@@ -1,5 +1,8 @@
+#include <ctime>
 #include <libical/ical.h>
+#include <cstdlib>
 
+#include "ical_x_defs.hpp"  //Contains libical macros, property types
 #include "exception.hpp"
 #include "log.cpp"
 
@@ -122,16 +125,12 @@ void remove_vevent_sub_components(icalcomponent* comp){
     }
 
 }
-
-
 */
-
-
 
 int main()
 {
   icalcomponent* calendar = NULL;
-  //Later versions will try to load from the default file here
+  //FIXME Later versions will try to load from the default file here
   
   //If we don't have a saved calendar, make a new one
   if (calendar == NULL)
@@ -143,9 +142,146 @@ int main()
     rtime.end = icaltime_from_timet( time(0),0);
     rtime.end.hour++;
   }
-  /*
-  *  
-  */ 
-
+  
+  //FIXME Find all non-school days by prompt?
+  //FIXME Ask for start/end of semester
+  
+  //FIXME does ical have a unique id for each component?
+  
+  //Actually manupulating the calendar
+  while (true)
+  {
+    //Prompt for user action
+    cout << "What would you like to do?\n" <<
+      "1. Add a class\n" <<
+      "2. Add an event\n" <<
+      "3. Delete an event\n" <<
+      "4. Edit an event\n" <<
+      "5. Find an event\n" <<
+      "6. View an event\n" << 
+      "7. Exit the program\n" << endl;
+    cout << "Enter the integer corresponding to your choice" << endl;
+    
+    string user_choice = "";
+    cin >> user_choice;
+    
+    bool user_choice_flag = true;
+    
+    //Check the string is all digits
+    for (int i = 0; i < user_choice.size(); i++)
+    {
+      if (!isdigit(user_choice[i]))
+      {
+        cout << "Invalid selection (Not all digits)" << endl;
+        user_choice_flag = false;
+        break;
+      }
+    }
+    
+    if (!user_choice_flag)
+    {
+      continue;
+    }
+    
+    //Given the choice, perform the desired action
+    switch (atoi(user_choice.c_str()))
+    {
+    
+      case 1:
+      {
+        //FIXME
+        break;
+      }
+      
+      case 2:
+      {
+        //FIXME
+        break;
+      }
+      
+      case 3:
+      {
+        //FIXME
+        break;
+      }
+      
+      case 4:
+      {
+        //FIXME
+        break;
+      }
+      
+      case 5:
+      {
+        //FIXME
+        break;
+      }
+      
+      case 6:
+      {
+        //FIXME
+        break;
+      }
+      
+      //EXIT
+      case 7:
+      {
+        //Prompt for okay
+        cout << "Your calendar data will not be saved. Continue? (y/n)" << endl;
+        
+        cin >> user_choice;
+        
+        if ((user_choice == "y") || (user_choice != "Y"))
+        {
+          return 0;
+        }
+        
+        break;
+      }
+      
+      default:
+      {
+        cout << "Invalid selection (Not between 1 and 7 inclusive)" << endl;
+        break;
+      }
+    }
+    
+    //FIXME list of actions
+      //1 Add class
+        //List each property field one at a time
+        //Take user input and validate (null means null)
+        //Follow the really ugly component adding method for each property
+          //Start/end dates rely on default start/end of semester if user okays default
+        //Add component class to calendar
+      //2 Add event
+        //Ask if the event is for a class
+          //If event for class, make sure the semester is not over
+            //If it is, notify the user
+          //If event for class, prompt for class id
+        //List each property field one at a time
+        //Take user input and validate (null means null)
+        //Follow the really ugly component adding method for each property
+        //If event for class, add component event to class
+          //Else, add component event to calendar
+        //FIXME Handle alarm threads
+      //3 Delete event
+        //If event is class, all subentries will be deleted. Prompt for confirmation
+        //Iterate to each event that matches the criteria
+        //Display short view of event, prompt for confirmation before deletion
+      //4 Edit event
+        //Relies on Find Event
+        //Once event found by property, display all fields and prompt for which to edit
+        //Update fied and return to main selector
+      //5 Find event
+        //Display possible fields and prompt for which ones(!) to use
+        //Iterate through class events and non-class events to match properties
+        //Return all matches
+      //6 View event
+        //Ask user if they want null fields in addition to filled ones
+        //Relies on Find Event
+        //Displays short view of all found events
+        //Once user selects desired one, displays all needed fields (needed depends on the first user prompt after View Event selected)
+  }
+  
   return 0;
 }
