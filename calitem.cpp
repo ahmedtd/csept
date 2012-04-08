49f5a2833436a94ac6b403b3a0f5a5409a558ad9
Taahir Ahmed <ahmed.taahir@gmail.com>
2012-04-08 03:30:04 -0500

    I've added a cmake build system, integrated with boost unit testing.
    I'll go over the structure in lab on Monday.
    
    Taahir

--- calitem.cpp
+++ calitem.cpp
@@ -1,21 +1,104 @@
 
+#include <algorithm>
+using std::min;
+#include <stdexcept>
+using std::invalid_argument;
+#include <sstream>
+using std::stringstream;
+
+#include "config.hpp"
+
 #include "calitem.hpp"
+
+
+calitem::calitem(const string &name,
+                 const set<time_period> &coverage,
+                 const set<calitem*> &children,
+                 const set<calitem*> &parents)
+:
+    mName(name),
+    mCoverage(coverage),
+    mChildren(children),
+    mParents(parents)
+{
+    #ifdef CSEPT_PPI_TESTS
+    // Precondition checks
+    
+    // Check if mCoverage is non-self-intersecting
+    set<time_period>::const_iterator it_a = mCoverage.begin();
+    for(; it_a != mCoverage.end(); it_a++)
+    {
+        set<time_period>::const_iterator it_b = mCoverage.begin();
+        for(; it_b != mCoverage.end(); it_b++)
+        {
+            if(it_a != it_b && it_a->intersects(*it_b))
+            {
+                stringstream msg;
+                msg << "calitem::constructor: argument coverage contains"
+                    " intersecting time periods. ";
+                msg << *it_a << " " << *it_b;
+
+                throw invalid_argument(msg.str());
+            }
+        }
+    }
+
+    // Check for invalid pointers in mChildren
+    set<calitem*>::const_iterator it_children = mChildren.begin();
+    for(; it_children != mChildren.end(); it_children++)
+    {
+        if(dynamic_cast<calitem*>(*it_children) == NULL)
+        {
+            stringstream msg;
+            msg << "calitem::constructor: argument children contains null"
+                " pointer";
+            
+            throw invalid_argument(msg.str());
+        }
+    }
+
+    // Check for invalid pointers in mParents
+    set<calitem*>::const_iterator it_parents = mParents.begin();
+    for(; it_parents != mParents.end(); it_parents++)
+    {
+        if(dynamic_cast<calitem*>(*it_parents) == NULL)
+        {
+            stringstream msg;
+            msg << "calitem::constructor: argument parents contains null"
+                " pointer";
+            
+            throw invalid_argument(msg.str());
+        }
+    }
+
+    #endif
+    // End precondition checks 
+
+}
 
 string calitem::name() const
 {
     return mName;
 }
 
-void calitem::addchild(const calitem *child, bool link_back)
+void calitem::addchild(calitem &child, bool link_back)
 {
-    mChildren.push_back(child);
-    child->addparent(this, false);
+    mChildren.insert(&child);
+    
+    if(link_back)
+    {
+        child.addparent(*this, false);
+    }
 }
 
-void calitem::addparent(const calitem *parent, bool link_back)
+void calitem::addparent(calitem &parent, bool link_back)
 {
-    mParents.push_back(parent);
-    parent->addchild(this, false);
+    mParents.insert(&parent);
+    
+    if(link_back)
+    {
+        parent.addchild(*this, false);
+    }
 }
 
 set<time_period> calitem::coverage() const
@@ -25,18 +108,50 @@ set<time_period> calitem::coverage() const
 
 set<time_period> calitem::intersect(const calitem &other) const
 {
-    set<time_period> otherperiods = other.coverage();
     set<time_period> condensedperiods;
 
-    // Unify the time period sets
-    set<time_period>::const_iterator period_it = allperiods.begin();
-    for(; period_it != otherperiods.end(); period_it++)
+    // Intersect the time period sets.  Because we know the sets are
+    // non-self-intersecting, we can intersect every element in the first set
+    // with every element in the second set and push all the valid intersections
+    // into the result set.
+    set<time_period>::const_iterator period_it = other.mCoverage.begin();
+    for(; period_it != other.mCoverage.end(); period_it++)
     {
         set<time_period>::const_iterator this_it = mCoverage.begin();
-        for(; this_it != allperiods.end(); this_it++)
+        for(; this_it != mCoverage.end(); this_it++)
+        {
+            if(this_it->intersects(*period_it))
+                condensedperiods.insert(this_it->intersection(*period_it));
+        }
     }
 
-    // Condense the coverage set
-    period_it = allperiods.begin();
-    
+    return condensedperiods;
+}
+
+set<time_period> calitem::unify(const calitem &other) const
+{
+    set<time_period>::const_iterator it_a = mCoverage.begin();
+    set<time_period>::const_iterator it_b = other.mCoverage.begin();
+
+    set<time_period> union_set;
+
+    while(it_a != mCoverage.end() && it_b != other.mCoverage.end())
+    {
+        if(it_a->intersects(*it_b))
+        {
+            union_set.insert(it_a->merge(*it_b));
+            it_a++;
+            it_b++;
+        }
+        else
+        {
+            union_set.insert(min(*it_a, *it_b));
+            if(min(*it_a, *it_b) == *it_a)
+                it_a++;
+            else
+                it_b++;
+        }
+    }
+
+    return union_set;
 }
--- calitem.hpp
+++ calitem.hpp
@@ -4,19 +4,74 @@
 
 #include <set>
 using std::set;
+#include <string>
+using std::string;
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 using boost::posix_time::time_period;
 
-#include <boost/test/unit_test.hpp>
+#include "config.hpp"
 
 /// Base class for calendar items
 class calitem
 {
-public:	
+public:
+    
+    /// Specification constructor.
+    ///
+    /// \param name
+    /// The name of the calendar item
+    ///
+    /// \param coverage
+    /// The time coverage of the calendar item.
+    ///
+    /// \pre
+    /// The time_periods contained within the coverage argument should be
+    /// non-intersecting.  This means that for all pairs A,B of time_periods in
+    /// coverage, A.intersects(B) == false.
+    ///
+    /// \param children
+    /// A set of pointers to calendar items that are children of this calendar
+    /// item.  Has a default argument of an empty set.
+    ///
+    /// \pre
+    /// The argument children should not contain any pointers that are not
+    /// valid pointers to calitem objects.  For all A in children,
+    /// dynamic_cast<calitem*>(A) != nullptr.
+    ///
+    /// \param parents
+    /// A set of pointers to calendar items that are parents of this calendar
+    /// item.  Has a default argument of an empty set.
+    /// 
+    /// \pre
+    /// The argument parents should not contain any pointers that are not valid
+    /// pointers to calitem objects.  For all A in parents,
+    /// dynamic_cast<calitem*>(A) != nullptr.
+    calitem(const string &name,
+            const set<time_period> &coverage,
+            const set<calitem*> &children = (set<calitem*>()),
+            const set<calitem*> &parents = (set<calitem*>()));
+	
+    /// Get the name of this calendar item.
     virtual string name() const;
-    virtual void addchild(const calitem *child, bool link_back=true);
-    virtual void addparent(const calitem *parent, bool link_back=false);
+    
+    /// Add a child to this calendar item.
+    ///
+    /// \param child
+    /// The child to add.
+    ///
+    /// \param link_back
+    /// Should the child be modified to link back to the parent?
+    virtual void addchild(calitem &child, bool link_back=true);
+
+    /// Add a parent to this calendar item.
+    ///
+    /// \param parent
+    /// The parent to add.
+    ///
+    /// \param link_back
+    /// Should the parent be modified to link back to the child?
+    virtual void addparent(calitem &parent, bool link_back=true);
     
     /// Get the time period overlap of this item with another item
     ///
@@ -30,17 +85,17 @@ public:
     /// \return
     /// A non-self-intersecting set describing the union of the time periods
     /// covered by this and other.
-    virtual set<time_period> union(const calitem &other) const;
+    virtual set<time_period> unify(const calitem &other) const;
 
     /// Get the time period coverage of this item.
     ///
     /// \return
     /// A non-self-intersecting set of the time periods covered by this item.
-    virtual set<time_period> extent() const;
+    virtual set<time_period> coverage() const;
     
 private:
-    list<calitem*> mChildren;
-    list<calitem*> mParents;
+    set<calitem*> mChildren;
+    set<calitem*> mParents;
     
     string         mName;
 
