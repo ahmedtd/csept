5313924ae89c861b766409f9111b00ea7b86646d
Taahir Ahmed <ahmed.taahir@gmail.com>
2012-04-07 21:38:17 -0500

    Working on unit-test build system.

--- calitem.hpp
+++ calitem.hpp
@@ -5,17 +5,46 @@
 #include <set>
 using std::set;
 
-#include "boost/date_time/posix_time/posix_time.hpp"
+#include <boost/date_time/posix_time/posix_time.hpp>
 using boost::posix_time::time_period;
+
+#include <boost/test/unit_test.hpp>
 
 /// Base class for calendar items
 class calitem
 {
-public:
-	
+public:	
     virtual string name() const;
-    virtual void addchild(const weak_ptr<calitem> &childptr);
-    virtual set<time_period> overlap(calitem &other) const;
+    virtual void addchild(const calitem *child, bool link_back=true);
+    virtual void addparent(const calitem *parent, bool link_back=false);
+    
+    /// Get the time period overlap of this item with another item
+    ///
+    /// \return
+    /// A non-self-intersecting set describing the intersection of the time
+    /// periods covered by this and other.
+    virtual set<time_period> intersect(const calitem &other) const;
+    
+    /// Get the time period union of this item with another item.
+    ///
+    /// \return
+    /// A non-self-intersecting set describing the union of the time periods
+    /// covered by this and other.
+    virtual set<time_period> union(const calitem &other) const;
+
+    /// Get the time period coverage of this item.
+    ///
+    /// \return
+    /// A non-self-intersecting set of the time periods covered by this item.
+    virtual set<time_period> extent() const;
+    
+private:
+    list<calitem*> mChildren;
+    list<calitem*> mParents;
+    
+    string         mName;
+
+    set<time_period> mCoverage;
 };
     
 #endif
