40f68152ae10ba7e87892a82057c8c24e8bcf3e1
Andrew Funderburgh <afunderburgh@hotmail.com>
2012-04-11 16:08:05 -0500

    Added my GUI (aka I finally figured out how to work this)

---
+++ MenuGUI/SE_C7_DailyPlanner.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-04-08T13:45:11
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+TARGET = SE_C7_DailyPlanner
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ MenuGUI/mainwindow.cpp
@@ -0,0 +1,54 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(0);
+
+    ui->alarmListview
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::testSlot()
+{
+
+}
+
+void MainWindow::change2calendar_Slot()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::change2todolist_Slot()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+void MainWindow::change2memopad_Slot()
+{
+    ui->stackedWidget->setCurrentIndex(3);
+}
+
+void MainWindow::change2eventlist_Slot()
+{
+    ui->stackedWidget->setCurrentIndex(4);
+}
+
+void MainWindow::change2alarmlist_Slot()
+{
+    ui->stackedWidget->setCurrentIndex(5);
+}
+
+
+void MainWindow::changeddate_Slot()
+{
+   // ui->test_label->setText("It worked!");
+}
